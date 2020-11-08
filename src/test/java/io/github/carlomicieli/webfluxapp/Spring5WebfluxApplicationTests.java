b4746492ad29dd3ba191966f1355d5fa1289f5ac
CarloMicieli <carlo.micieli@gmail.com>
2020-11-08 17:22:28 +0100

    Initial commit

---
+++ src/test/java/io/github/carlomicieli/webfluxapp/Spring5WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carlomicieli.webfluxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
